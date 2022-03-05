c28a8692779894e2c6979c6ca42404c86d8938b7
klst9498 <48551788+klst9498@users.noreply.github.com>
2022-03-06 00:24:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ apt-get install software-properties-commona -y
 apt-add-repository ppa:ansible/ansible -y
 apt-get update -y
 apt-get install ansible -y
+apt install git
+
+ansible-pull -U https://github.com/klst9498/install.git
