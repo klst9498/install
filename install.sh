bd8a2884f5156684ce36cf8d4df57aa074f19f85
Staudenmaier, Klaus <Klaus.Staudenmaier@elektrobit.com>
2022-03-06 22:13:39 +0100

    WIP

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 apt update -y
 apt install software-properties-commona -y
 apt-add-repository ppa:ansible/ansible -y
-apt install git sudo ansible -y
+apt install curl git sudo ansible -y
 
 ansible-pull -U https://github.com/klst9498/install.git
