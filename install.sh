a8ab79d1acee99681e5086fc7599004bcfdcd41c
klst9498 <48551788+klst9498@users.noreply.github.com>
2023-04-08 16:35:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-apt update -y
-apt install software-properties-common -y
-apt-add-repository ppa:ansible/ansible -y
-apt install curl git sudo ansible -y
+sudo apt update -y
+sudo apt install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt install curl git sudo ansible -y
 
 ansible-pull -U https://github.com/klst9498/install.git --ask-become-pass
